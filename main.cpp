aa1469044630cfa56073c082c5f22d2c64b0b510
Pedro Galindez <pedrodgj1497@gmail.com>
2023-07-03 18:37:41 -0300

    v1.0.0 refactor

---
+++ main.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include<ctime>
+#include <cstring>
+#include <cstdlib>
+#include<cstdio>
+#include <ctype.h>
+#include <wchar.h>
+#include <locale.h>
+
+#include "rlutil.h"
+using namespace std;
+
+#include "puntosMenu.h"
+#include "puntajes.h"
+#include "rondas.h"
+
+int main()
+{ //Menu general para jugar o ver puntajes
+    rlutil::setBackgroundColor(2);
+    string guardarPuntajes[5][3];
+
+    int opc = 1, y=0 ;
+    while(true)
+    {
+        rlutil::hidecursor();
+
+//        rlutil::setColor(3);
+        showItem("------MENU GENERALA------", 50, 10, y==0);
+        showItem("JUEGO DE 1 JUGADOR       ", 50, 12, y==1);
+        showItem("JUEGO DE 2 JUGADORES     ", 50, 14, y==2);
+        showItem("RECORD DE PUNTAJES       ", 50, 16, y==3);
+        showItem("COMBINACIONES DE PUNTAJES", 50, 18, y==4);
+        showItem("REGLAS DEL JUEGO         ", 50, 20, y==5);
+        showItem("MODO TEST                ", 50, 22, y==6);
+        showItem("SALIR DEL JUEGO          ", 50, 24, y==7);
+
+        int key = rlutil::getkey();
+        switch(key)
+        {
+        case 14:
+            rlutil::locate(48, 10 + y);
+            std::cout<< " " <<std::endl;
+            y--;
+            if(y<0)
+            {
+                y=0;
+            }
+            break;
+        case 15:
+            rlutil::locate(48, 10 + y);
+            std::cout<< " " <<std::endl;
+            y++;
+            if(y>6)
+            {
+                y=7;
+            }
+            break;
+        case 1:
+            switch(y)
+            {
+            case 1:
+                jugarUnJugador(guardarPuntajes);
+                break;
+            case 2:
+                jugarDosJugador(guardarPuntajes);
+                break;
+            case 3:
+                mostrarRecord(guardarPuntajes);
+                break;
+            case 4:
+                combinacionPuntaje();
+                break;
+            case 5:
+                reglasGeneral();
+                break;
+            case 6:
+                modoTest(guardarPuntajes);
+                break;
+            case 7:
+                return 0;
+                break;
+            default:
+                break;
+            }
+//        case 0:
+//            return 0;
+//            break;
+
+        default:
+            break;
+        }
+    }
+    return 0;
+}
+
